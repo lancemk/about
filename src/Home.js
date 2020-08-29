3fbe701d4cf6b6a76b09d32d32af301d7285be03
Lance Mok <bochuan-mok@e.ntu.edu.sg>
2020-08-29 17:18:49 +0800

    added css

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './main.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+
+      <div className="container" >
+
+        <div className="left-space">
+          Empty
+        </div>
+
+        <div className="left">
+          This is Vertical Nav bar
+        </div>
+
+        <div className="center">
+          This is Home Page of Lance Mok
+        </div>
+
+        <div className="right">
+          Other tabs
+        </div>
+
+        <div className="right-space">
+          Empty
+        </div>
+
+      </div>
+     
+    )
+  }
+}
