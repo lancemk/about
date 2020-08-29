5cda0f929b646ce9f917ce75c5cf04c495b5deb4
Lance Mok <bochuan-mok@e.ntu.edu.sg>
2020-08-29 17:03:08 +0800

    Updates

---
+++ precache-manifest.5238ef672fc76e7781219b481779d7bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147048341e6771634fa70ae7ad7f466b",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "623cc5c1ba3be26412c1",
+    "url": "/aboutme/static/js/2.39b45aef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aboutme/static/js/2.39b45aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cdfd44208e583f0363",
+    "url": "/aboutme/static/js/main.38110ea3.chunk.js"
+  },
+  {
+    "revision": "955f3070b93d413f0fd9",
+    "url": "/aboutme/static/js/runtime-main.95483440.js"
+  }
+]);
