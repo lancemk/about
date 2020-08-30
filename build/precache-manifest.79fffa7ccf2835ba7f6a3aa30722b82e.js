d186f659f1eb65c8a3a1fbd73fa25fc357f4b4f9
Lance Mok <bochuan-mok@e.ntu.edu.sg>
2020-08-30 13:44:52 +0800

    containered layout

---
+++ build/precache-manifest.79fffa7ccf2835ba7f6a3aa30722b82e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9273c6870df5954fd86305477e5539d0",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "12093c48ef6f4642965b",
+    "url": "/about/static/css/main.dea8e224.chunk.css"
+  },
+  {
+    "revision": "d2e9f3e3d3f14035f5ff",
+    "url": "/about/static/js/2.9fe72d43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/about/static/js/2.9fe72d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12093c48ef6f4642965b",
+    "url": "/about/static/js/main.ceee0a74.chunk.js"
+  },
+  {
+    "revision": "7d785bd1b70ae97e89e3",
+    "url": "/about/static/js/runtime-main.b078b8ed.js"
+  }
+]);
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import { Card , Paper, List } from '@material-ui/core'
 import './main.css'
 
 export default class Home extends Component {
   render() {
     return (
       <>
-      <div> Out of Container </div>
       <div className="container" >
 
         <div className="left-space">
@@ -13,15 +13,72 @@ export default class Home extends Component {
         </div>
 
         <div className="left">
-          This is Vertical Nav bar
+          <Card style={{padding: "1.5em"}}> 
+          <h2> Lance B.C. Mok </h2> <img src="./image/lance.png" />
+          <div >
+            <h4 style={{fontWeight: "700"}}> Tools of Experience </h4>
+            <List>
+            online web apps, OpenGL, WebGL full stack development
+            electronics sensor design
+            robotics sensor API stacks: ROS framework, C++ API
+            </List>
+            
+            <h4 style={{fontWeight: "700"}  }> Education </h4>
+            <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+            <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+          </div>
+          </Card>
         </div>
 
         <div className="center">
-          This is Home Page of Lance Mok
+       
+          <Paper className="center-paper" style={{maxHeight: '600px', overflow: 'auto'}}>
+            <List style={{maxHeight: '100%', overflow: 'auto'}}>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            <Paper variant="outlined"  style={{background: "#a3a3a3", padding: "1.5em"}}>
+            <h4 style={{fontWeight: "700"}}> Education </h4>
+              <li> BEng. of Mechanical Engineering, Nanyang Technological University </li>
+              <li> Diploma in Mechanical Engineering, Singapore Polytechnic </li>
+            </Paper>
+            </List> 
+          </Paper>
+
         </div>
 
         <div className="right">
-          Other tabs
+          <Paper variant="outlined" >
+
+
+          </Paper>
         </div>
 
         <div className="right-space">
